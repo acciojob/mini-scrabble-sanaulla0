f808abdae5198794fc87358bda6a104294951d85
sanaulla0 <vadapallisanaulla@gmail.com>
2022-12-19 11:00:56 +0530

    p1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+
+let box = document.getElementById("evaluatedText")
+let countElement = document.getElementById("letterCount")
+// let count = countElement.innerHTML;
+
+function startcount(){
+     let count=box.value;
+     countElement.innerHTML=count.length;
+     console.log(count.length);
+}
+  
+
+box.addEventListener("keyup",startcount)
+
